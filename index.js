9c091ee41ff31e3ef8a5ac1d77be8b040d7f478b
jatinprime <jatinagrawal28112003@gmail.com>
2025-01-27 19:48:00 +0530

    Added dotenv and gitignore

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/login' , (req , res) =>{
+    res.send("Login route") ;
+})
+app.get('/sign' , (req , res) =>{
+    res.send("sign route") ;
+})
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
   
