717168b4e5d918fedbe739dbdbfbbedd0331c8e1
jatinprime <jatinagrawal28112003@gmail.com>
2025-01-28 22:43:07 +0530

    trying to connect frontend and backend in the connection test

---
+++ Connection_Test/backend/index.js
@@ -0,0 +1,44 @@
+const express = require('express') ;
+const app = express() ;
+// const connectDB = require('./config/db') ;
+// const dotenv = require('dotenv') ;
+// const productRoutes = require('./routes/productRoutes') ;
+
+// connectDB() ;
+// dotenv.config() ;
+
+const PORT = process.env.PORT || 3000 ;
+
+// app.use(express.json()) ;
+// -> /api/users -> this will send the thing to users
+
+app.get('/' , (req , res) => {
+    res.status(200).json({
+        message : "Successfully running"
+    })
+})
+app.get('/api/jokes' , (req , res)=>{
+    const jokes = [ 
+        {
+            id : 1 , 
+            title : "First JOke",
+            content : "This is first joke"
+        },
+        {
+            id : 2 , 
+            title : "Second JOke",
+            content : "This is second joke"
+        }
+    ]
+    res.send(jokes) ;
+})
+
+// app.use('/api' , productRoutes) ;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`) ;
+});
+
+
+
+
